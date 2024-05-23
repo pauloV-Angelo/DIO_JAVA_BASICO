9651cb274c4812828f9eef6f06cfb796de45bf07
Paulo Vitor <angelopaulovitor@gmail.com>
2024-05-23 13:26:54 -0300

    aula: tipos e variaveis atualizada

--- tipos-e-variaveis/src/application/TiposVariaveis.java
+++ tipos-e-variaveis/src/application/TiposVariaveis.java
@@ -3,8 +3,28 @@ package application;
 public class TiposVariaveis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//tipos numéricos
+		byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
+	
+	// tipos lógicos
+	boolean verdadeiro = true;
+	boolean falso = false;
+	
+	//tipos textuais 
+	char a = 'a';
+	String ola = "olá"; // String não é um tipo primitivo
+	
+	//variável vs constante
+	int numeroVariavel = 10;
+	numeroVariavel = 5;
+	final int NUMEROCONSTANTE = 10; //não pode ser alterado devido à palavra final
+	
 	}
 
 }
