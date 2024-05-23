3143df74e124174831ab5c4238c63cfc25c05276
Paulo Vitor <angelopaulovitor@gmail.com>
2024-05-23 13:01:22 -0300

    nova aula adicionada

---
+++ teste-gitignore/src/application/TesteGitignore.java
@@ -0,0 +1,12 @@
+package application;
+
+public class TesteGitignore {
+
+	public static void main(String[] args) {
+		/*
+		 * projeto criado para estudar o funcionamento do .gitignore
+		 */
+
+	}
+
+}
